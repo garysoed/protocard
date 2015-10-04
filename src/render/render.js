b8457518aa29bd4de6a0c56d70c0ffeef59df65c
Gary Soedarsono <garysoed@gmail.com>
2015-10-04 12:57:45 -0700

    basic renderer

---
+++ src/render/render.js
@@ -0,0 +1,11 @@
+var page = require('webpage').create();
+
+var baseDir = './src/render';
+
+page.viewportSize = { width: 825, height: 1125 };
+page.clipRect = { top: 0, left: 0, width: 825, height: 1125 };
+page.open(baseDir + '/template.html', function() {
+  page.injectJs(baseDir + '/extra.js');
+  page.render('test.png');
+  phantom.exit();
+});
