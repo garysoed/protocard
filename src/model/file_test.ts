219d617fb1de50afd036f38cde3dd73cdd4660bb
Gary Soedarsono <garysoed@gmail.com>
2016-01-02 15:34:13 -0800

    file test

--- src/model/file_test.ts
+++ src/model/file_test.ts
@@ -1,8 +1,15 @@
 import TestBase from '../testbase';
 
 import File, { FileTypes } from './file';
+import Serializable from './serializable';
 
 describe('model.File', () => {
+  it('should be able to be converted to / from JSON', () => {
+    let file = new File(FileTypes.TSV, 'content');
+    let copy = Serializable.fromJSON(Serializable.toJSON(file));
+    expect(copy).toEqual(file);
+  });
+
   describe('getType', () => {
     it('should return the correct file type', () => {
       expect(File.getType('file.csv')).toEqual(FileTypes.CSV);
