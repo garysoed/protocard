86de0c495f36130560abc86188ef552fdfc43c0d
Gary Soedarsono <garysoed@gmail.com>
2016-11-20 12:32:32 -0800

    backup

---
+++ api/test.js
@@ -0,0 +1,3 @@
+window['API_KEY'] = 'AIzaSyC_xLbtB1l2PUk3m8IGM7cPf7JbjYFyUW0';
+window['CLIENT_ID'] = '20382560326-329ecu1t7rn2sind9ccd3gf765p6q6rb.apps.googleusercontent.com';
+window['SECRET'] = 'kXVE-CFkHbueHiGravw11jhK';
